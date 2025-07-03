57ce1119762c969c45a083ecade9c206cb4c4003
Roman Vasilyev <vasilyev-r@mail.ru>
2025-07-03 18:51:12 +0300

    Second commit

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 	
-	/*
-		Template Name: Главная
-		Template Post Type: page
-	*/
+	/**
+	 * Template Name: Главная
+	 * Template Post Type: page
+	 */
 	
-	//include 'header.php';
 	session_start();
 	
 	if ( isset( $_SESSION['win'] ) ) {
@@ -14,7 +13,6 @@
 	} else $display = "none";
 	
 ?>
-
 <!doctype html>
 <html lang="ru">
 	<head>
